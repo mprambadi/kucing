ec8eb2be7913995c5ad8470c80df7dea973a91f0
mprambadi <mprambadi@gmail.com>
2017-12-04 20:37:06 +0700

    config change

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/kucing3';
+$config['base_url'] = 'http://localhost:9000/kucing3';
 
 /*
 |--------------------------------------------------------------------------
